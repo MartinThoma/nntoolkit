887d59385b15cc17f1552d4f5d0459ef8c7e1bb5
Martin Thoma <info@martin-thoma.de>
2015-01-08 19:40:38 +0100

    nntoolkit/train.py: Added command line parameters for training

--- nntoolkit/train.py
+++ nntoolkit/train.py
@@ -31,18 +31,29 @@ def get_parser():
                         metavar="FILE",
                         type=lambda x: utils.is_valid_file(parser, x),
                         required=True)
+    parser.add_argument("--batchsize",
+                        dest="batch_size",
+                        help=("A positive number which indicates how many "
+                              "training examples get looked at before the "
+                              "parameters get updated."),
+                        default=256,
+                        type=int)
+    parser.add_argument("-lr", "--learningrate",
+                        dest="learning_rate",
+                        help=("A positive number, typically between 0 and "
+                              "10."),
+                        default=0.1,
+                        type=float)
     return parser
 
 
 def minibatch_gradient_descent(model_file,
-                               training_data,
                                x,
                                y,
                                batch_size=256,
                                lr=0.1):
     """Train a given neural network.
     :param model_file: Path to the model.tar
-    :param training_data: Path to a data.tar
     :param x: A numpy array with features
     :param y: The list of classes
     :param batch_size: A positive integer which defines after how many training
@@ -92,9 +103,24 @@ def minibatch_gradient_descent(model_file,
     # TODO: Update parameters, save it?
 
 
-def main(model_file, training_data):
-    minibatch_gradient_descent(model_file, training_data)
+def get_data(training_data):
+    """Get data as x and y numpy arrays
+    :param training_data: The path to a tar file
+    """
+    # TODO: Fill x and y with data from tar / HDF5 files
+    x = np.array([])
+    y = np.array([])
+    return (x, y)
+
+
+def main(model_file, training_data, batch_size, learning_rate):
+    """Train model_file with training_data."""
+    x, y = get_data(training_data)
+    minibatch_gradient_descent(model_file, x, y, batch_size, learning_rate)
 
 if __name__ == '__main__':
     args = get_parser().parse_args()
-    main(args.model_file, args.training_data)
+    main(args.model_file,
+         args.training_data,
+         args.batch_size,
+         args.learning_rate)
